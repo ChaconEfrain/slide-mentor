dc7cf7da1b9ca012bba1089ac0c250de74a531c2
efrachaga <efrachaga@gmail.com>
2025-04-23 21:40:19 +0000

    Add first draft of tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,59 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  firstName: varchar({ length: 255 }).notNull(),
-  lastName: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  createdAT: timestamp("created_at").notNull().defaultNow(),
+  updatedAT: timestamp("updated_at").notNull().$onUpdate(() => new Date()),
 });
 
+export const presentations = pgTable("presentations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer('user_id').references(() => users.id, { onDelete: 'cascade'}).notNull(),
+  title: varchar('title', { length: 255 }).notNull(),
+  description: varchar('description', { length: 255 }),
+  prompt: varchar('prompt', { length: 255 }).notNull(),
+  createdAT: timestamp("created_at").notNull().defaultNow(),
+  updatedAT: timestamp("updated_at").notNull().$onUpdate(() => new Date()),
+});
+
+export const prompts = pgTable("prompts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer('user_id').references(() => users.id, { onDelete: 'cascade'}).notNull(),
+  presentationId: integer('presentation_id').references(() => presentations.id).notNull(),
+  prompt: text('prompt').notNull(),
+  createdAT: timestamp("created_at").notNull().defaultNow(),
+  updatedAT: timestamp("updated_at").notNull().$onUpdate(() => new Date()),
+});
+
+/* Relations */
+export const usersRelations = relations(users, ({ many }) => ({
+  presentations: many(presentations),
+  prompts: many(prompts),
+}))
+
+export const presentationsRelations = relations(presentations, ({ one, many }) => ({
+  user: one(users, {
+    fields: [presentations.userId],
+    references: [users.id],
+  }),
+  prompts: many(prompts),
+}))
+
+export const promptsRelations = relations(prompts, ({ one }) => ({
+  user: one(users, {
+    fields: [prompts.userId],
+    references: [users.id],
+  }),
+  presentation: one(presentations, {
+    fields: [prompts.presentationId],
+    references: [presentations.id],
+  }),
+}))
+
 export type User = typeof users.$inferSelect;
+export type Presentation = typeof presentations.$inferSelect;
+export type Prompt = typeof prompts.$inferSelect;
