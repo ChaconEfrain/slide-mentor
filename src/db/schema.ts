ac704db8629c7b0cee6e2fe8966f30e7ba132eec
efrachaga <efrachaga@gmail.com>
2025-04-23 18:35:59 +0000

    Add drizzleORM

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  firstName: varchar({ length: 255 }).notNull(),
+  lastName: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export type User = typeof users.$inferSelect;
