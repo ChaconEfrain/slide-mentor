614f41d6ee20e24e9696edf44af8c2e6905dce74
ChaconEfrain <efrachaga@gmail.com>
2025-04-25 07:05:30 -0600

    Refactor ESLint configuration to use compat.config and enhance rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          trailingComma: "all",
+          semi: false,
+          printWidth: 80,
+          tabWidth: 2,
+          endOfLine: "auto",
+        },
+      ],
+      "react/no-escape-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
